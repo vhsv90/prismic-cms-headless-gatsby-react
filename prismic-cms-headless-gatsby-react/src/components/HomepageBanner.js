da6fceea72eb4904856424e1dee5c465132ce3ff
Victor Sanchez <vhsanchez90@hotmail.com>
2021-06-10 17:52:11 -0600

    home page updated to use data from prismic

--- prismic-cms-headless-gatsby-react/src/components/HomepageBanner.js
+++ prismic-cms-headless-gatsby-react/src/components/HomepageBanner.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { RichText } from 'prismic-reactjs'
 
-import homeBanner from '../images/home-banner.jpg'
-
-const HomepageBanner = () => (
+const HomepageBanner = ({ bannerContent }) => (
   <section
     className="homepage-banner"
-    style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(${homeBanner})` }}
+    style={{
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(${bannerContent.background.url})`,
+    }}
   >
     <div className="banner-content container">
-      <h2 className="banner-title">John Doe</h2>
+      <h2 className="banner-title">
+        {RichText.asText(bannerContent.title.raw)}
+      </h2>
       <p className="banner-description">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque
-        volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh,
-        viverra non, semper suscipit, posuere a, pede asdf.
+        {RichText.asText(bannerContent.description.raw)}
       </p>
-      <Link to="/about" className="banner-button">Learn More</Link>
+      <Link to={bannerContent.link.url} className="banner-button">
+        {RichText.asText(bannerContent.linkLabel.raw)}
+      </Link>
     </div>
   </section>
 )
--- prismic-cms-headless-gatsby-react/src/components/SliceZone.js
+++ prismic-cms-headless-gatsby-react/src/components/SliceZone.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import FullWidthImage from './slices/FullWidthImage'
+import ImageGallery from './slices/ImageGallery'
+import ImageHighlight from './slices/ImageHighlight'
+import Quote from './slices/Quote'
+import Text from './slices/Text'
 
 const SliceZone = ({ sliceZone }) => {
-    return sliceZone.map((slice, index) => (
-        <p key={index}>{slice.slice_type}</p>
-    ))
+
+    const sliceComponents = {
+        full_width_image: FullWidthImage,
+        image_gallery: ImageGallery,
+        image_highlight: ImageHighlight,
+        quote: Quote,
+        text: Text,
+    }
+
+    const sliceZoneContent = sliceZone.map((slice, index) => {
+        const SliceComponent = sliceComponents[slice.slice_type]
+        if(SliceComponent) {
+            return <SliceComponent slice={slice} key={`slice-${index}`} />
+        }
+        return null;
+    })
+
+    return <main className="container">{sliceZoneContent}</main>
 }
 
 export default SliceZone
